5cd9e103b34c418c862cc7d99ec6eeccdba28d27
Taissera <taissera@ya.ru>
2022-11-20 02:32:58 +0300

    5_Ex_ConditionIfElse + ToLower

---
+++ Examples/5_Ex_ConditionIfElse/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Console.Write("Введите username:");
+string username = Console.ReadLine();
+if(username == "Маша")
+{
+    Console.WriteLine("Ура! Цэ ж Маша!");
+}
+else
+{
+    Console.Write("Ну, привет ");
+    Console.Write(username);
+}
+*/
+
+/**/
+Console.Write("Введите username:");
+string username = Console.ReadLine();
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура! Цэ ж МАША!");
+}
+else
+{
+    Console.Write("Ну, привет ");
+    Console.Write(username);
+}
+/**/
