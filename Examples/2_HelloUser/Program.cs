6489461e052b7ba9974c7355014b0331cb1af8cf
Taissera <taissera@ya.ru>
2022-11-20 00:26:46 +0300

    update for HelloUser

--- Examples/2_HelloUser/Program.cs
+++ Examples/2_HelloUser/Program.cs
@@ -1,6 +1,11 @@
-﻿// Нам необходимо создать "контейнер" для хранения данных, которые вводит пользователь
+﻿/* Нам необходимо создать "контейнер" для хранения данных, которые вводит пользователь
 Console.WriteLine("Введите ваше имя");
 string? username = Console.ReadLine();
 // Далее выводим приветствие в консоль
 Console.WriteLine("Привет, ");
 Console.WriteLine(username);
+// Воспользуемся командой Write, чтобы не переходить на новую строку */
+Console.WriteLine("Введите ваше имя");
+string? username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(username);
