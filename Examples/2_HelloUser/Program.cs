5af4d4aa21b679bd65b7110c6a93af3b72abb11e
Taissera <taissera@ya.ru>
2022-11-19 19:02:00 +0300

    hello user

---
+++ Examples/2_HelloUser/Program.cs
@@ -0,0 +1,6 @@
+﻿// Нам необходимо создать "контейнер" для хранения данных, которые вводит пользователь
+Console.WriteLine("Введите ваше имя");
+string? username = Console.ReadLine();
+// Далее выводим приветствие в консоль
+Console.WriteLine("Привет, ");
+Console.WriteLine(username);
