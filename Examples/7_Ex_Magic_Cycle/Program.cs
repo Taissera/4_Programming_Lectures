7a6f5980bfdccc913afe8ef3bf8e850a8504d518
Taissera <taissera@ya.ru>
2022-11-20 17:01:48 +0300

    commit for 7_Ex_Magic_Cycle

--- Examples/7_Ex_Magic_Cycle/Program.cs
+++ Examples/7_Ex_Magic_Cycle/Program.cs
@@ -1,8 +1,25 @@
-﻿/*
-Console.Clear();
-*/
-Console.Set.CursorPosition(10,4);
+﻿Console.Clear();
+/*
+Console.SetCursorPosition(10,4);
 Console.WriteLine("+");
-
+*/
+//int xa = 1;
+int xa = 40;
+int ya = 1;
+int xb = 1;
+int yb = 30;
+//int xc = 40;
+//int xc = 60;
+int xc = 80;
+int yc = 30;
+// также можно записать как: int xa = 1, ya = 1, xb = 1, yb = 30;
+Console.SetCursorPosition(xa,ya);
+Console.WriteLine("+");
+Console.SetCursorPosition(xb,yb);
+Console.WriteLine("+");
+Console.SetCursorPosition(xc,yc);
+Console.WriteLine("+");
+// Сделаем, чтобы вершина (xa,ya) была посередине --> xa = 40
+// Получим похожее на равнобедренный треугольник
 
 
