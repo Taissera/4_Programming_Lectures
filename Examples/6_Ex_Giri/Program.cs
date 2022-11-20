9e31d0641b184a8d9a8929b74a21d6ba4e3a5933
Taissera <taissera@ya.ru>
2022-11-20 15:06:55 +0300

    commit  6_Ex_Giri

---
+++ Examples/6_Ex_Giri/Program.cs
@@ -0,0 +1,16 @@
+﻿// В нашем примере с гирями фигурировали конкрертные числа, пропишем их
+int a = 1;
+int b = 2;
+int c = 6;
+int d = 8;
+int e = 4;
+// определим переменную max, в которую кладём значение первой гири
+int max = a;
+// далее пропишем набор условий
+if(a > max) max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+if(d > max) max = d;
+if(e > max) max = e;
+Console.Write("max = ");
+Console.Write(max);
